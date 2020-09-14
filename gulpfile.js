f7fb7cdba5ebde9c56d401d3f47ad7f7aff026f1
yuliasigova <yulya12yulya@gmail.com>
2020-09-14 21:59:05 +0300

    Автоматизировала сборку проекта

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
 
 // Styles
 
@@ -16,19 +20,67 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("./build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// HTML
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(gulp.dest("./build"))
+    .pipe(sync.stream());
+}
+exports.html = html;
+
+const webp =() => {
+  return gulp.src("source/img/*.webp")
+  .pipe(gulp.dest("./build/img"))
+}
+exports.webp = webp;
+
+//Imagemin
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("./build/img"));
+}
+exports.images = images;
+
+// Copy
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/js/**",
+  ], {
+    base: 'source'
+  })
+    .pipe(gulp.dest("build"));
+};
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+  return del("build");
+};
+exports.clean = clean;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: "./build"
     },
     cors: true,
     notify: false,
@@ -42,10 +94,13 @@ exports.server = server;
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+  gulp.watch('source/*.html', gulp.series(html));
 }
 
-exports.default = gulp.series(
-  styles, server, watcher
-);
+// Build
+const build = gulp.series(clean, copy, styles, images, webp, html);
+exports.build = build;
+
+// Start
+exports.start = gulp.series(build, server, watcher);
