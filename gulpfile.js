5519ed57105de39a0f859aa51d9d11c6ed08e7e3
yuliasigova <yulya12yulya@gmail.com>
2020-09-15 12:42:08 +0300

    Исправила замечания

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("./build/css"))
     .pipe(csso())
     .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
